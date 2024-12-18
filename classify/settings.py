a19ca7e18581fb54b01290e09c71b2387a50cf34
JB <135088138+JanardanBhetwal@users.noreply.github.com>
2024-12-18 23:48:05 +0545

    Update settings.py

--- classify/settings.py
+++ classify/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-*5y_erf%$k-768qc3qd17-w9aw^l_e20x*g3e$!4vvthqb9&*@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["https://animal-classification-lrf6.onrender.com/"]
+ALLOWED_HOSTS = ["animal-classification-lrf6.onrender.com"]
 
 
 # Application definition
