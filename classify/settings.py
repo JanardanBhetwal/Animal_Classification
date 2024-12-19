f8842d232d350cbc4c9d22766d9f1a5e4a04aa33
JB <135088138+JanardanBhetwal@users.noreply.github.com>
2024-12-19 20:23:35 +0545

    Added csrf_trusted_source in settings.py

--- classify/settings.py
+++ classify/settings.py
@@ -113,7 +113,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+CSRF_TRUSTED_ORIGINS = ['https://animal-classification-lrf6.onrender.com/']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
